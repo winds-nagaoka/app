c072c619e2100329042e8b0413cd9b70a6421c42
ryo4004 <kato@ryo4004.net>
2020-04-30 23:54:53 +0900

    Update source

--- asset/archive/concert/other.js
+++ asset/archive/concert/other.js
@@ -151,6 +151,37 @@ const otherConcert = [
       {audio: 1, video: 1, title: 'ウェールズの歌より第二楽章', composer: 'A.デイヴィス'},
       {audio: 2, video: 2, title: 'スクーティン・オン・ハードロック', composer: 'D.R.ホルジンガー'}
     ]
+  }, {
+//////////////////////////////////
+////// スプリングコンサート //////
+//////////////////////////////////
+    id: 'other003',
+    type: 'other',
+    title: 'スプリングコンサート',
+    time: {
+      timestamp: 1570942800,
+      date: '2020年5月9日(土)',
+      time: '16:00',
+      label: '開演',
+    },
+    place: ['長岡リリックホール', 'コンサートホール'],
+    conductor: [{name: '河本隆吉'}],
+    poster: 'https://winds-n.com/img/poster/other/2020.jpg',
+    guide: 'https://winds-n.com/spring2020',
+    contents: [
+      {label: '第1部', music: [0, 1, 2]},
+      {label: '第2部', music: [3, 4]},
+      {label: 'アンコール', music: [5, 6]},
+    ],
+    data: [
+      {title: '五月の風', composer: '真島俊夫'},
+      {title: '詩的間奏曲', composer: 'J.バーンズ'},
+      {title: '祝典のための音楽', composer: 'P.スパーク', movement: ['第1楽章 con brio', '第2楽章 andantino', '第3楽章 vivo']},
+      {title: '「スラヴ舞曲」より', composer: 'A.ドヴォルザーク', arranger: '河本隆吉', movement: ['作品46 第2番', '作品72 第2番', '作品46 第8番']},
+      {title: '富士山～北斎の版画に触発されて～', composer: '真島俊夫'},
+      {title: 'ロマネスク', composer: 'J.スウェアリンジェン'},
+      {title: '木陰の散歩道', composer: 'E.F.ゴールドマン'}
+    ]
   }
 ]
 
