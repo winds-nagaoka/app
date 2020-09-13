34119113ac5bccf52b4d55c9d67e29e0171f808e
ryo4004 <kato@ryo4004.net>
2020-09-13 19:05:57 +0900

    Update source

--- asset/source/source.js
+++ asset/source/source.js
@@ -1,27 +1,24 @@
 const sourceList = [
   {
-    id: '2020spring',
-    title: 'スプリングコンサート',
+    id: 'main033',
+    title: '第33回定期演奏会',
     type: 'source',
     time: {
-      timestamp: 1589007600,
-      date: '2020年5月9日(土)',
-      time: '16:00'
+      timestamp: 1602392400,
+      date: '2020年10月11日(日)',
+      time: '14:00'
     },
     sourceStatus: false,
     contents: [
       {label: '第1部', music: [0, 1, 2]},
-      {label: '第2部', music: [3, 4]},
-      {label: 'アンコール', music: [5, 6]},
+      {label: 'アンコール', music: [3, 4]},
     ],
     data: [
       {audio: 0, title: '五月の風', composer: '真島俊夫'},
       {audio: 1, title: '詩的間奏曲', composer: 'J.バーンズ'},
-      {audio: 2, title: '祝典のための音楽', composer: 'P.スパーク'},
-      {audio: 5, title: 'スラブ舞曲', composer: 'A.ドヴォルザーク'},
-      {audio: 8, title: '富士山 〜北斎の版画に触発されて〜', composer: '真島俊夫'},
-      {audio: 9, title: 'ロマネスク', composer: 'J.スウェアリンジェン'},
-      {audio: 10, title: '木陰の散歩道', composer: 'E.F.ゴールドマン'}
+      {audio: 2, title: 'スラブ舞曲', composer: 'A.ドヴォルザーク'},
+      {audio: 5, title: 'ロマネスク', composer: 'J.スウェアリンジェン'},
+      {audio: 6, title: 'ブロックM', composer: 'J.H.ビリック'}
     ]
   }
 ]
--- asset/source/sourceAudio.js
+++ asset/source/sourceAudio.js
@@ -1,20 +1,16 @@
 const sourceAudio = [
   {
-    id: '2020spring',
+    id: 'main033',
     status: true,
-    directory: 'source_2020spring/',
+    directory: 'source_2020winds/',
     data: [
       {available: true, data: 0, path: 'may.mp3'},
       {available: true, data: 1, path: 'poeticintermezzo.mp3'},
-      {available: true, data: 2, path: 'musicforafestival-001.mp3', addtitle: '第1楽章 Con brio'},
-      {available: true, data: 2, path: 'musicforafestival-002.mp3', addtitle: '第2楽章 Andantino'},
-      {available: true, data: 2, path: 'musicforafestival-003.mp3', addtitle: '第3楽章 Vivo'},
-      {available: true, data: 3, path: 'slavonic46-2.mp3', addtitle: '作品46 第2番'},
-      {available: true, data: 3, path: 'slavonic72-2.mp3', addtitle: '作品72 第2番'},
-      {available: true, data: 3, path: 'slavonic46-8.mp3', addtitle: '作品46 第8番'},
-      {available: true, data: 4, path: 'fuji.mp3'},
-      {available: true, data: 5, path: 'romanesque.mp3'},
-      {available: true, data: 6, path: 'onthemall.mp3'},
+      {available: true, data: 2, path: 'slavonic46-2.mp3', addtitle: '作品46 第2番'},
+      {available: true, data: 2, path: 'slavonic72-2.mp3', addtitle: '作品72 第2番'},
+      {available: true, data: 2, path: 'slavonic46-8.mp3', addtitle: '作品46 第8番'},
+      {available: true, data: 3, path: 'romanesque.mp3'},
+      {available: true, data: 4, path: 'blockm.mp3'},
     ]
   }
 ]
