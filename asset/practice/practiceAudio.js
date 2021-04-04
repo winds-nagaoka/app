4fcb890b7e441b7aa49c0f88fea81e4dc05e5c18
ryo4004 <kato@ryo4004.net>
2021-04-05 00:21:13 +0900

    Update practice

--- asset/practice/practiceAudio.js
+++ asset/practice/practiceAudio.js
@@ -1,4 +1,52 @@
-const practiceAudio = []
+const practiceAudio = [
+  {
+    id: '20210403',
+    directory: '20210403/',
+    file: [
+      {label: '炎', path: '001.mp3'},
+      {label: 'Happiness', path: '002.mp3'},
+      {label: 'アフリカン・シンフォニー', path: '003.mp3'},
+      {label: 'スペイン', path: '004.mp3'},
+      {label: '虹', path: '005.mp3'},
+      {label: 'となりのトトロ', path: '006.mp3'},
+      {label: '合奏後半', path: '007.mp3'}
+    ],
+    contents: [
+      {
+        file: 0,
+        list: []
+      },
+      {
+        file: 1,
+        list: []
+      },
+      {
+        file: 2,
+        list: []
+      },
+      {
+        file: 3,
+        list: []
+      },
+      {
+        file: 4,
+        list: []
+      },
+      {
+        file: 5,
+        list: []
+      },
+      {
+        file: 6,
+        list: [
+          {time: '12:54', label: 'スペイン'},
+          {time: '50:12', label: '虹'},
+          {time: '55:00', label: 'となりのトトロ'},
+        ]
+      },
+    ]
+  },
+]
 
 module.exports = {
   practiceAudio
