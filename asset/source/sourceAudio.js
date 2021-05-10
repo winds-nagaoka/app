683d04fc4df710157091ebf53d6a7733ec0a034d
ryo4004 <kato@ryo4004.net>
2021-05-10 21:32:08 +0900

    Update source

--- asset/source/sourceAudio.js
+++ asset/source/sourceAudio.js
@@ -1,21 +1,5 @@
 const sourceAudio = [
   {
-    id: 'mini2021',
-    status: true,
-    directory: 'source_mini2021/',
-    data: [
-      {available: true, data: 0, path: 'hapiness.mp3'},
-      {available: true, data: 1, path: 'spain.mp3'},
-      {available: true, data: 2, path: 'homura.mp3'},
-      {available: true, data: 3, path: 'disneyvillainsmedley.mp3'},
-      {available: true, data: 4, path: 'connpassofyourheart.mp3'},
-      {available: true, data: 5, path: 'makeyouhappy.mp3'},
-      {available: true, data: 6, path: 'yorunikakeru.mp3'},
-      {available: true, data: 7, path: 'totoro.mp3'},
-      {available: true, data: 8, path: 'niji.mp3'},
-      {available: true, data: 9, path: 'africansymphony.mp3'},
-    ],
-  }, {
     id: 'main034',
     status: true,
     directory: 'source_2021winds/',
