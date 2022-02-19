20b2a78e42aaa9523a9661f01a51d31dc5da1f36
ryo4004 <kato@ryo4004.net>
2022-02-19 09:53:01 +0900

    Update source

--- asset/source/sourceAudio.js
+++ asset/source/sourceAudio.js
@@ -15,7 +15,23 @@ const sourceAudio = [
       {available: true, data: 8, path: 'shiroi.mp3'},
       {available: true, data: 9, path: 'matsuken.mp3'},
     ],
-  },
+  }, {
+    id: 'main035',
+    status: true,
+    directory: 'source_2022winds/',
+    data: [
+      {available: true, data: 0, path: 'thunderbirds.mp3'},
+      {available: true, data: 1, path: 'songofsea.mp3'},
+      {available: true, data: 2, path: 'sentochihiro.mp3'},
+      {available: true, data: 3, path: 'musicforafestival1.mp3', addtitle: '第1楽章 con brio'},
+      {available: true, data: 3, path: 'musicforafestival2.mp3', addtitle: '第2楽章 andantino'},
+      {available: true, data: 3, path: 'musicforafestival3.mp3', addtitle: '第3楽章 vivo'},
+      {available: true, data: 4, path: 'cortege.mp3'},
+      {available: true, data: 5, path: 'fontanediroma.mp3'},
+      {available: true, data: 6, path: 'yellmarch.mp3'},
+      {available: true, data: 8, path: 'blockm.mp3'},
+    ],
+  }
 ]
 
 module.exports = {
