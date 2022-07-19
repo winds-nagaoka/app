7f5b0db6e05a432b9aa19aa8d689b87ca9c9c44c
ryo4004 <kato@ryo4004.net>
2022-07-19 17:46:35 +0900

    Update source

--- asset/source/source.js
+++ asset/source/source.js
@@ -1,31 +1,5 @@
 const sourceList = [
   {
-    id: 'mini2022',
-    title: '春のミニコンサート2022',
-    type: 'source',
-    time: {
-      timestamp: 1651899600,
-      date: '2022年5月7日(土)',
-      time: '14:00',
-    },
-    sourceStatus: true,
-    contents: [
-      {label: 'プログラム', music: [0, 1, 2, 3, 4, 5, 6, 7]},
-      {label: 'アンコール', music: [8, 9]},
-    ],
-    data: [
-      {audio: 0, title: 'ドラゴンクエストより序曲', composer: 'すぎやまこういち'},
-      {audio: 1, title: 'Dear Mr.Jones', arranger: '熱帯JAZZ楽団'},
-      {audio: 2, title: 'ツバメ', composer: 'YOASOBI'},
-      {audio: 3, title: 'ヒットソング・プレイバック90s'},
-      {audio: 4, title: 'ディープパープルメドレー'},
-      {audio: 5, title: 'アルデバラン', composer: 'AI'},
-      {audio: 6, title: 'ルパン三世メドレー'},
-      {audio: 7, title: 'ディズニーメドレーII'},
-      {audio: 8, title: '白い恋人たち', composer: 'フランシス・レイ'},
-      {audio: 9, title: 'マツケンサンバII', composer: '宮川彬良'},
-    ],
-  }, {
     id: 'main035',
     title: '第35回定期演奏会',
     type: 'source',
--- asset/source/sourceAudio.js
+++ asset/source/sourceAudio.js
@@ -1,21 +1,5 @@
 const sourceAudio = [
   {
-    id: 'mini2022',
-    status: true,
-    directory: 'source_mini2022/',
-    data: [
-      {available: true, data: 0, path: 'dragonquest.mp3'},
-      {available: true, data: 1, path: 'dearmrjones.mp3'},
-      {available: true, data: 2, path: 'tsubame.mp3'},
-      {available: true, data: 3, path: 'hitsong90.mp3'},
-      {available: true, data: 4, path: 'deeppurple.mp3'},
-      {available: true, data: 5, path: 'aldebaran.mp3'},
-      {available: true, data: 6, path: 'rupin.mp3'},
-      {available: true, data: 7, path: 'disney.mp3'},
-      {available: true, data: 8, path: 'shiroi.mp3'},
-      {available: true, data: 9, path: 'matsuken.mp3'},
-    ],
-  }, {
     id: 'main035',
     status: true,
     directory: 'source_2022winds/',
