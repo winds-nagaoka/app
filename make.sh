1ad376f430b4b13e1e6f51266ad4fe2e82d80ee5
ryo4004 <kato@ryo4004.net>
2022-12-30 00:09:27 +0900

    実行場所を変更

--- make.sh
+++ make.sh
@@ -2,9 +2,9 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/concert.ts
-./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/concert_media.ts
-./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/practice.ts
-./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/practice_media.ts
-./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/source.ts
-./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/source_media.ts
+${SCRIPT_DIR}/node_modules/.bin/ts-node ${SCRIPT_DIR}/script/concert.ts
+${SCRIPT_DIR}/node_modules/.bin/ts-node ${SCRIPT_DIR}/script/concert_media.ts
+${SCRIPT_DIR}/node_modules/.bin/ts-node ${SCRIPT_DIR}/script/practice.ts
+${SCRIPT_DIR}/node_modules/.bin/ts-node ${SCRIPT_DIR}/script/practice_media.ts
+${SCRIPT_DIR}/node_modules/.bin/ts-node ${SCRIPT_DIR}/script/source.ts
+${SCRIPT_DIR}/node_modules/.bin/ts-node ${SCRIPT_DIR}/script/source_media.ts
