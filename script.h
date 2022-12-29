1c231fe1bb89fec302ef1720faf07837481d56d8
ryo4004 <kato@ryo4004.net>
2022-12-29 20:16:06 +0900

    ts-nodeで実行する

--- script.h
+++ script.h
@@ -2,9 +2,9 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-node ${SCRIPT_DIR}/script/concert.js
-node ${SCRIPT_DIR}/script/concert_media.js
-node ${SCRIPT_DIR}/script/practice.js
-node ${SCRIPT_DIR}/script/practice_media.js
-node ${SCRIPT_DIR}/script/source.js
-node ${SCRIPT_DIR}/script/source_media.js
+./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/concert.ts
+./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/concert_media.ts
+./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/practice.ts
+./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/practice_media.ts
+./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/source.ts
+./node_modules/.bin/ts-node ${SCRIPT_DIR}/script/source_media.ts
